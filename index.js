3b5355daa5867ae4d296e3362eed68e035d8d4fb
Allison Homem <mrs.allisonhomem@outlook.com>
2021-07-13 16:38:54 -0500

    finished task 5

--- index.js
+++ index.js
@@ -205,11 +205,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
-}
+function miles(kilos){
+  return kilos * 0.621371;
+};
 
-
+console.log(miles(5));
 
 //Task 5b - Feet to CM
 /*
@@ -219,11 +219,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
-
+console.log(feet(200));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
